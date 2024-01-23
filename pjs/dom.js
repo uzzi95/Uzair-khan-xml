1c49261d3144fa33a0eb7a36f0b5e2256e1293da
uzair <uzairkhan2194@gmail.com>
2024-01-23 20:32:24 +0500

    dom

---
+++ pjs/dom.js
@@ -0,0 +1,47 @@
+// DOM ----> DOCUMENT OBJECT MODEL
+let elem = document.getElementById('second');
+//console.log(elem);
+
+//elem.innerHTML = "AN APPLE A DAY"
+elem.textContent = "AN APPLE A DAY";
+elem.style.backgroundColor = "orange";
+elem.style.textAlign = "center";
+elem.classList.add('world');
+elem.className = "Hello";
+elem.classList.remove('Hello');
+
+
+elem.classList.add("Wow");
+elem.classList.add("great");
+elem.classList.add("yes");
+elem.classList.remove('yes');
+
+let obj = document.getElementsByClassName('container');
+
+obj[0].innerHTML = "HELLO WORLD";
+obj[0].style.backgroundColor = "pink";
+obj[0].style.textAlign = "center";
+obj[0].style.color = "navy";
+obj[0].style.fontWeight = "italic";
+obj[0].style.fontSize = "40px";
+
+obj[0].classList.add('wow');
+obj[0].classList.add('now');
+obj[0].classList.add('how');
+
+let newElem = document.createElement('p');
+
+obj[0].appendChild(newElem);
+
+newElem.innerHTML = "hello";
+newElem.style.backgroundColor = "yellow";
+newElem.style.textAlign = "center";
+newElem.style.color = "green";
+newElem.style.fontWeight = "bold";
+newElem.style.fontSize = "30px";
+
+
+let para = document.createElement('h3');
+newElem.appendChild(para);
+
+
--- pjs/seventh.js
+++ pjs/seventh.js
@@ -1,9 +1,22 @@
-let grade = prompt("Enter Your Grade");
-let per = parseInt(prompt("Enter your perc"))
+// let grade = prompt("Enter Your Grade");
+// let per = parseInt(prompt("Enter your perc"))
 
 // TURNARY OPERATION
 // let result = grade == "A" || grade == "B" || grade == "C" || grade == "D" ? "Pass" : "Fail";
 // console.log(result);
 
-let result = per === 90 && grade == "A" || per === 80 && grade == "B" ? "PASS" : "FAIL";
+// let result = per === 90 && grade == "A" || per === 80 && grade == "B" ? "PASS" : "FAIL";
+// console.log(result);
+
+let grade = prompt("ENTER YOUR GRADE");
+console.log(grade);
+console.log(typeof(grade));
+
+let per =parseInt (prompt("Enter Your per"));
+console.log(typeof(per));
+console.log(per);
+
+// TURNARY OPERATIONS
+
+let result = grade === "A" && per === 90 || grade === "B"  && per === 80 || grade === "C" && per === 70 ? "PASS" : "FAIL";
 console.log(result);
