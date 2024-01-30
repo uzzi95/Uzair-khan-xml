554c26f45924df5c0356bacd69813c49744b2904
uzair <uzairkhan2194@gmail.com>
2024-01-30 20:09:16 +0500

    save bulb

---
+++ regex-form/task 1/30/2024/light.js
@@ -0,0 +1,29 @@
+let btn = document.getElementById('toggleBtn');
+let bulb = document.getElementById('bulb');
+let mq = document.getElementById('mq');
+btn.addEventListener('click' , toggleBulb);
+function toggleBulb(){
+
+    if(btn.textContent.includes('On')  && mq.textContent.includes('On')){
+        bulb.src = "on.png";
+        btn.textContent = "Turn Off";
+        mq.textContent = "Turn Off";
+        btn.className = `btn btn-outline-success`;
+        btn.style.boxShadow = "3px 3px 4px 4px green";
+        mq.style.color = "yellow";
+        mq.style.boxShadow = "3px 3px 4px 4px green";
+        bulb.style.boxShadow = "3px 3px 4px 4px green"
+        bulb.style.padding = "10px"
+    }
+    else{
+        bulb.src = "off.png";
+        btn.textContent = "Turn On";
+        mq.textContent = "Turn On";
+        btn.className = `btn btn-outline-danger`;
+        btn.style.boxShadow = "3px 3px 4px 4px red";
+        mq.style.color = "red";
+        mq.style.boxShadow = "3px 3px 4px 4px red";
+        bulb.style.boxShadow = "3px 3px 4px 4px red"
+        bulb.style.padding = "10px"
+    }
+}
